983fe4b8cf711641585254531c0ab1c9a0c9995f
Derek <derekbrimley@gmail.com>
2017-02-01 14:45:57 -0700

    volunteer start

--- client/app/volunteer/index.js
+++ client/app/volunteer/index.js
@@ -1,17 +1,81 @@
 'use strict';
+
 const angular = require('angular');
-
 const uiRouter = require('angular-ui-router');
-
 import routes from './routes';
+import _ from 'lodash';
 
 export class VolunteerController {
 
-  constructor(Auth, $state, $http) {
-      'ngInject';
-    //   Array of sample data
-      this.data = [{attribute1:"A value",attribute2:"Another value"},{attribute1:"a value for the second object",attribute2:"Another value for the second object"}]
+    list='volunteers';
+    user = {
+        name: '',
+        email: '',
+        password: '',
+        type:'student'
+    };
+    submitted = false;
 
+    constructor(Auth, User, $state, $http) {
+        'ngInject';
+        this.Auth = Auth;
+        // Use the User $resource to fetch all users
+        var ctrl = this;
+        this.volunteers = User.query(function(volunteers){
+            console.log("Volunteers",volunteers);
+            ctrl.volunteers = _.filter(volunteers,{role:'volunteer'})
+        })
+        this.volunteer_requests = User.query(function(volunteer_requests) {
+            ctrl.volunteer_requests = _.filter(volunteer_requests,{role:'volunteer'});
+        })
+        this.organizations = [
+          {
+              name:"Trader Joes",
+              date_added:"12/03/16",
+              desc:"Brings food during coding classes."
+          },
+          {
+              name:"Adobe",
+              date_added:"05/03/16",
+              desc:"Provides volunteers for bi-weekly coding class"
+          }
+        ]
+
+        this.$http = $http;
+    }
+
+    delete(user) {
+        user.$remove();
+        this.volunteers.splice(this.volunteers.indexOf(user), 1);
+    }
+
+    create(form) {
+        var ctrl = this;
+        this.submitted = true;
+        
+        if(form.$valid) {
+            return this.Auth.createUser({
+                name: this.user.name,
+                email: this.user.email,
+                password: this.user.password,
+                type: this.user.type
+            })
+            .then(() => {
+                this.$state.go('volunteer');
+            })
+            .catch(err => {
+                err = err.data;
+                this.erros = [];
+                
+                if(err.name) {
+                    angular.forEach(err.fields, field => {
+                        console.log(field,ctrl.errors);
+                        ctrl.errors.push(err)
+                    //   this.errors[field] = err.message;
+                    });
+                }
+            });
+        }
     }
 }
 
