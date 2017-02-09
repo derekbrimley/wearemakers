f7d9954fbb9c0fe62b6d2be5b0ef80289aec616a
Derek <derekbrimley@gmail.com>
2017-02-08 17:42:35 -0700

    update volunteers

--- server/api/volunteer/index.js
+++ server/api/volunteer/index.js
@@ -8,6 +8,7 @@ var router = express.Router();
 
 router.get('/', auth.hasRole('admin'), controller.index);
 router.get('/:id', auth.hasRole('admin'), controller.show);
+router.get('/:id/getClasses', auth.hasRole('admin'), controller.getClasses)
 // router.post('/', auth.isAuthenticated(),controller.create);
 router.put('/:id', auth.isAuthenticated(), controller.update);
 router.delete('/:id', auth.hasRole('admin'), controller.destroy);
