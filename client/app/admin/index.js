622abb7f451fe1ee630dea4000665d102f495cd0
Derek <derekbrimley@gmail.com>
2017-02-22 17:43:56 -0700

    added fields for volunteer in signup, hid volunteer class requests

--- client/app/admin/index.js
+++ client/app/admin/index.js
@@ -9,7 +9,7 @@ import students from './students';
 import volunteers from './volunteers';
 import reports from './reports';
 import classEditor from '../../components/classEditor';
-export default angular.module('refugeeApp.admin', ['refugeeApp.auth', 'ui.router',classes, classEditor, volunteers, students, scheduling, reports])
+export default angular.module('refugeeApp.admin', ['refugeeApp.auth', 'ui.router', classes, classEditor, volunteers, students, scheduling, reports])
   .config(routes)
   .controller('AdminController', AdminController)
   .name;
