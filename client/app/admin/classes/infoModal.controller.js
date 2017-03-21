c10ba7b8cf77d880381c4f50e215cfd54a28ea3d
John Blackburn <jblackburn@xerva.com>
2017-03-20 18:14:14 -0600

    Adding reporting to class tab

--- client/app/admin/classes/infoModal.controller.js
+++ client/app/admin/classes/infoModal.controller.js
@@ -11,9 +11,37 @@ export class InfoModalController {
         console.log(this.$resolve.class);
         this.$http.get('/api/classes/getStudentsFromClass/' + this.$resolve.class._id)
         .then(function(res) {
-            console.log("STUDENTS", res);
+            // console.log("STUDENTS", res);
             ctrl.students = res.data;
         })
+
+        var attendance = {}
+        var count;
+        ctrl.$http.get('/api/classes/reports/attendance/'+this.$resolve.class._id)
+        .then(function(res){
+            attendance={
+                data : res.data,
+                absent : "",
+                attended : "",
+                excused : "",
+                attendancePercent: ""
+            }
+        },function(err){
+            console.log("ERR",err);
+        })
+        .then(function(){
+            for(count = 0; count < attendance.data.length; count++){
+                if(attendance.data[count].attendance == 'Absent'){
+                    attendance.absent = Number(attendance.data[count].count)
+                } else if (attendance.data[count].attendance == 'Attended'){
+                    attendance.attended = Number(attendance.data[count].count)
+                } else if (attendance.data[count].attendance == 'Excused'){
+                    attendance.excused = Number(attendance.data[count].count)
+                } 
+            }
+            attendance.attendancePercent = (100 * (attendance.attended  / (attendance.absent + attendance.attended + attendance.excused))).toFixed(2)
+            ctrl.attendance = attendance
+        });
     }
 
 
